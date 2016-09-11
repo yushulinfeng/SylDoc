930a80e3106a16111d3c1be5091a3d20c648fd5f
LinFeng <china2012syl@163.com>
2016-09-12 02:30:04 +0800

    初步完善登录注册体系

--- SylDoc/action/UserAct.py
+++ SylDoc/action/UserAct.py
@@ -4,12 +4,12 @@
 '''用户相关'''
 from SylDoc.utils.check import checkUser, getPassWord
 from SylDoc.utils.tools import webResponse
-from SylDoc.models import User, WebManager
+from SylDoc.models import User
 
 '''
 用户注册
 send:username,userpass
-resp:1-succ,-1-name,-2-pass,-3-exist
+resp:1-succ,-1-name,-2-pass,-3-exist/other error
 '''
 def userRegister(request):  # 用户注册
     # 获取参数
@@ -40,81 +40,15 @@ def userLogin(request):  # 用户登录
     userpass = request.POST.get('userpass', '')
     state = checkUser(username, userpass)
     if state:
-        return webResponse(str(-1))
+        return webResponse("-1")
+    state = -1
     # 用户密码加密
     userpass = getPassWord(userpass)
     # 核对数据库
     db = User.objects.filter(username=username, userpass=userpass)
-    state = -1
-    for u in db:
-        if u != None:
-            state = 1
-            break
-    if state == 1:
+    if db.exists():  # 数据存在，即登录成功
+        state = 1
         request.session['username'] = username
     return webResponse(str(state))
-
-def managerLogin(request):  # 管理员登录
-        # 获取参数
-    username = request.POST.get('username', '')
-    userpass = request.POST.get('userpass', '')
-    state = checkUser(username, userpass)
-    if state:
-        return webResponse(str(-1))
-    # 用户密码加密
-    userpass = getPassWord(userpass)
-    # 核对数据库
-    db = WebManager.objects.filter(username=username, userpass=userpass)
-    usertype = 0
-    state = -1
-    for u in db:
-        if u != None:
-            state = 1
-            usertype = u.usertype
-            break
-    if state == 1:
-        request.session['usertype'] = usertype
-    return webResponse(str(state))
-
-def managerAdd(request):  # 添加管理员
-    usertype = request.session['usertype']
-    if(usertype == None):  # 未登录
-        return webResponse(str(-1))
-        # 获取参数
-    username = request.POST.get('username', '')
-    userpass = request.POST.get('userpass', '')
-    newtype = request.POST.get('usertype', '')
-    state = checkUser(username, userpass)
-    if state:
-        return webResponse(str(state))
-    # 用户密码加密
-    userpass = getPassWord(userpass)
-    # 存入数据库
-    db = WebManager(username=username, userpass=userpass, usertype=newtype)
-    try:
-        db.save()  # 这句是可能出现异常的，void方法，只能try-catch
-        state = 1
-    except:  # 已注册用户由于违反unique而抛出异常
-        state = -3
-    return webResponse(str(state))
-
-def managerDel(request):  # 删除管理员
-    usertype = request.session['usertype']
-    if(usertype == None):  # 未登录
-        return webResponse(str(-1))
-    pass
-
-def managerAlter(request):  # 修改管理员类型
-    usertype = request.session['usertype']
-    if(usertype == None):  # 未登录
-        return webResponse(str(-1))
-    pass
-
-def managerAll(request):  # 获取所有管理员
-    usertype = request.session['usertype']
-    if(usertype == None):  # 未登录
-        return webResponse(str(-1))
-    pass
-
 
 
---
+++ SylDoc/action/WebManagerAct.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''管理员相关'''
+
+from SylDoc.utils.check import checkUser, getPassWord
+from SylDoc.utils.tools import webResponse
+from SylDoc.models import  WebManager
+
+'''
+管理员登录
+send:username,userpass
+resp:1-succ,-1-fail
+'''
+def managerLogin(request):  # 管理员登录
+    # 获取参数
+    username = request.POST.get('username', '')
+    userpass = request.POST.get('userpass', '')
+    state = checkUser(username, userpass)
+    if state:
+        return webResponse("-1")
+    # 用户密码加密
+    userpass = getPassWord(userpass)
+    # 核对数据库
+    db = WebManager.objects.filter(username=username, userpass=userpass)
+    usertype = 0
+    state = -1
+    for u in db:
+        if u != None:
+            state = 1
+            usertype = u.usertype
+            break
+    if state == 1:
+        request.session['usertype'] = usertype
+    return webResponse(str(state))
+
+'''添加管理员
+send:username,userpass,usertype
+resp:1-succ,-1-login,-2-power,-3-format,-4-error
+'''
+def managerAdd(request):  # 添加管理员
+    usertype = request.session['usertype']
+    if(usertype == None):  # 未登录
+        return webResponse("-1")
+    if(usertype != '0' & usertype != '1'):  # 权限不够
+        return webResponse("-2")
+    # 获取参数
+    username = request.POST.get('username', '')
+    userpass = request.POST.get('userpass', '')
+    newtype = request.POST.get('usertype', '')
+    if checkUser(username, userpass):
+        return webResponse("-3")
+    # 用户密码加密
+    userpass = getPassWord(userpass)
+    # 存入数据库
+    db = WebManager(username=username, userpass=userpass, usertype=newtype)
+    try:
+        db.save()  # 这句是可能出现异常的，void方法，只能try-catch
+        return webResponse("1")
+    except:  # 已注册用户由于违反unique而抛出异常
+        return webResponse("-4")
+
+'''
+删除管理员
+send:username
+resp:1-succ,-1-login,-2-power,-3error
+'''
+def managerDel(request):  # 删除管理员
+    usertype = request.session['usertype']
+    if(usertype == None):  # 未登录
+        return webResponse(str(-1))
+    if(usertype != '0' & usertype != '1'):  # 权限不够
+        return webResponse(str(-2))
+    username = request.POST.get('username', '')
+    # 查找数据库
+    db = WebManager.objects.filter(username=username)
+    for u in db:
+        if u != None:
+            u.delete()
+            return webResponse("1")
+    return webResponse("-3")
+
+'''
+修改管理员类型
+send:username,usertype
+resp:1-succ,-1-login,-2-power,-3error
+'''
+def managerAlter(request):  # 修改管理员类型
+    usertype = request.session['usertype']
+    if(usertype == None):  # 未登录
+        return webResponse("-1")
+    if(usertype != '0' & usertype != '1'):  # 权限不够
+        return webResponse("-2")
+    username = request.POST.get('username', '')
+    newtype = request.POST.get('usertype', '')
+    # 更新数据库（先获取，在保存也可）
+    db = WebManager.objects.filter(username=username)
+    try:
+        db.update(usertype=newtype)
+        return webResponse("1")
+    except:   
+        return webResponse("-3")
+
+def managerAll(request):  # 获取所有管理员
+    usertype = request.session['usertype']
+    if(usertype == None):  # 未登录
+        return webResponse("-1")
+    # 核对数据库
+    db = WebManager.objects.all()
+    res = "["
+    for u in db:
+        if u != None:
+            if res != "[" :
+                res += ","
+            res += "{'name':'" + u.username + "','type':'" + u.usertype + "'}";
+    res += "]"
+    return webResponse(res)
+
+
+
